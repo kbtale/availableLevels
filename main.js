f784b03241a38f54801a68c42a8b8b4e894fc6f3
Carlos Bolívar <36908755+kbtale@users.noreply.github.com>
2022-11-10 23:36:10 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
 document.getElementById("default").click();
 
-function openCity(evt, cityName) {
+var prevInside = false;
+
+function openCity(evt, cityName, inside) {
     // Declare all variables
     var i, tabcontent, tablinks;
-  
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
+    tabcontenti = document.getElementsByClassName("tabcontentinside");
+    if (inside == false){
+        for (i = 0; i < tabcontent.length; i++) {
+            tabcontent[i].style.display = "none";
+        }
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinks = document.getElementsByClassName("tablinks");
+        for (i = 0; i < tablinks.length; i++) {
+          tablinks[i].className = tablinks[i].className.replace(" active", "");
+        }
+        for (i = 0; i < tabcontenti.length; i++) {
+            tabcontenti[i].style.display = "none";
+        }
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinksi = document.getElementsByClassName("tablinksinside");
+        for (i = 0; i < tablinksi.length; i++) {
+          tablinksi[i].className = tablinksi[i].className.replace(" active", "");
+        }
     }
-  
-    // Get all elements with class="tablinks" and remove the class "active"
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    else{
+        for (i = 0; i < tabcontenti.length; i++) {
+            tabcontenti[i].style.display = "none";
+        }
+        // Get all elements with class="tablinks" and remove the class "active"
+        tablinksi = document.getElementsByClassName("tablinksinside");
+        for (i = 0; i < tablinksi.length; i++) {
+          tablinksi[i].className = tablinksi[i].className.replace(" active", "");
+        }
     }
+
+    prevInside=inside;
   
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(cityName).style.display = "block";
